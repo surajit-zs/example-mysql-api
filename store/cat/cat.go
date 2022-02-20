7b404eb462d0b518fc18a2a3c7b51971e5249de7
surajit-zs <surajit.pore@zopsmart.com>
2022-02-20 15:55:48 +0530

    write store function

--- store/cat/cat.go
+++ store/cat/cat.go
@@ -1,6 +1,8 @@
 package cat
 
 import (
+	"database/sql"
+
 	"developer.zopsmart.com/go/gofr/pkg/errors"
 	"developer.zopsmart.com/go/gofr/pkg/gofr"
 
@@ -45,6 +47,23 @@ func (c catStore) Get(ctx *gofr.Context) ([]models.Cat, error) {
 	return cats, nil
 }
 
+func (c catStore) GetByID(ctx *gofr.Context, id string) (models.Cat, error) {
+	var resp models.Cat
+
+	query := "select * from cat where id=$1"
+	err := ctx.DB().QueryRowContext(ctx, query, id).Scan(&resp.ID, &resp.Name, &resp.Age)
+
+	if err == sql.ErrNoRows {
+		return models.Cat{}, errors.EntityNotFound{Entity: "cat", ID: id}
+	}
+
+	if err != nil {
+		return models.Cat{}, errors.DB{Err: err}
+	}
+
+	return resp, nil
+}
+
 func (c catStore) Create(ctx *gofr.Context, cat models.Cat) (models.Cat, error) {
 	query := "Insert into cat values(?,?,?)"
 	_, err := ctx.DB().ExecContext(ctx, query, cat.ID, cat.Name, cat.Age)
@@ -54,4 +73,34 @@ func (c catStore) Create(ctx *gofr.Context, cat models.Cat) (models.Cat, error) 
 	}
 
 	return cat, nil
+}
+
+func (c catStore) Update(ctx *gofr.Context, cat models.Cat) (models.Cat, error) {
+	query := "UPDATE cat SET name=$1, age=$2 WHERE id=$3"
+	_, err := ctx.DB().ExecContext(ctx, query, cat.Name, cat.Age, cat.ID)
+
+	if err == sql.ErrNoRows {
+		return models.Cat{}, errors.EntityNotFound{Entity: "cat", ID: cat.ID}
+	}
+
+	if err != nil {
+		return models.Cat{}, errors.DB{Err: err}
+	}
+
+	return cat, nil
+}
+
+func (c catStore) Delete(ctx *gofr.Context, id string) error {
+	query := "DELETE FROM cat where id=$1"
+	_, err := ctx.DB().ExecContext(ctx, query, id)
+
+	if err == sql.ErrNoRows {
+		return errors.EntityNotFound{Entity: "cat", ID: id}
+	}
+
+	if err != nil {
+		return errors.DB{Err: err}
+	}
+
+	return nil
 }
