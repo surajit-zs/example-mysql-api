24b55e041dc0652546bcd987c8dc9f28581d02dd
spore <surajit_pore@mcafee.com>
2022-02-21 12:28:45 +0530

    write service  testcases and func

--- services/cat/mysql.go
+++ services/cat/mysql.go
@@ -26,6 +26,20 @@ func (s Services) Get(ctx *gofr.Context) ([]models.Cat, error) {
 
 	return res, nil
 }
+
+func (s Services) GetByID(ctx *gofr.Context, id string) (models.Cat, error) {
+	if !idCheck(id) {
+		return models.Cat{}, errors.Error("validation error")
+	}
+
+	res, err := s.Store.GetByID(ctx, id)
+	if err != nil {
+		return models.Cat{}, errors.DB{Err: err}
+	}
+
+	return res, nil
+}
+
 func (s Services) Create(ctx *gofr.Context, c models.Cat) (models.Cat, error) {
 	if !check(c) {
 		return models.Cat{}, errors.Error("validation error")
@@ -39,18 +53,52 @@ func (s Services) Create(ctx *gofr.Context, c models.Cat) (models.Cat, error) {
 	return res, nil
 }
 
+func (s Services) Update(ctx *gofr.Context, c models.Cat) (models.Cat, error) {
+	if !check(c) {
+		return models.Cat{}, errors.Error("validation error")
+	}
+
+	res, err := s.Store.Update(ctx, c)
+	if err != nil {
+		return models.Cat{}, errors.DB{Err: err}
+	}
+
+	return res, nil
+}
+
+func (s Services) Delete(ctx *gofr.Context, id string) error {
+	if !idCheck(id) {
+		return errors.Error("validation error")
+	}
+
+	err := s.Store.Delete(ctx, id)
+	if err != nil {
+		return errors.DB{Err: err}
+	}
+
+	return nil
+}
+
 func check(c models.Cat) bool {
-	id, err := strconv.Atoi(c.ID)
+	if !idCheck(c.ID) {
+		return false
+	}
+
+	if c.Name == "" || c.Age <= 0 {
+		return false
+	}
+
+	return true
+}
+
+func idCheck(i string) bool {
+	id, err := strconv.Atoi(i)
 
 	if err != nil {
 		return false
 	}
 
 	if id <= 0 {
-		return false
-	}
-
-	if c.Name == "" || c.Age <= 0 {
 		return false
 	}
 
