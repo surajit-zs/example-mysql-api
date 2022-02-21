1ea1b9e80ceaf58f89743cf61c905d9f5a1d1218
spore <surajit_pore@mcafee.com>
2022-02-21 12:27:15 +0530

    write http testcases and func

--- http/cat/http.go
+++ http/cat/http.go
@@ -2,6 +2,7 @@ package cat
 
 import (
 	"net/http"
+	"strconv"
 
 	"developer.zopsmart.com/go/gofr/pkg/errors"
 	"developer.zopsmart.com/go/gofr/pkg/gofr"
@@ -49,6 +50,99 @@ func (h Handler) Create(ctx *gofr.Context) (interface{}, error) {
 	res := models.Response{
 		Cat:        resp,
 		Massage:    "created successful",
+		StatusCode: http.StatusCreated,
+	}
+
+	return res, nil
+}
+
+func (h Handler) GetByID(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	if id == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	_, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, errors.InvalidParam{
+			Param: []string{"id"},
+		}
+	}
+
+	res, err := h.Services.GetByID(ctx, id)
+	if err != nil {
+		return nil, errors.EntityNotFound{
+			Entity: "cat",
+			ID:     id,
+		}
+	}
+
+	resp := models.Response{
+		Cat:        res,
+		Massage:    "successful",
+		StatusCode: http.StatusOK,
+	}
+
+	return resp, nil
+}
+
+func (h Handler) Delete(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	if id == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	_, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, errors.InvalidParam{
+			Param: []string{"id"},
+		}
+	}
+
+	if err := h.Services.Delete(ctx, id); err != nil {
+		return nil, errors.DB{Err: err}
+	}
+
+	res := models.Response{
+		Cat:        nil,
+		Massage:    "successful",
+		StatusCode: http.StatusOK,
+	}
+
+	return res, nil
+}
+
+func (h Handler) Update(ctx *gofr.Context) (interface{}, error) {
+	i := ctx.PathParam("id")
+	if i == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	_, err := strconv.Atoi(i)
+	if err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	var c models.Cat
+
+	if err = ctx.Bind(&c); err != nil {
+		return nil, errors.InvalidParam{Param: []string{"body"}}
+	}
+
+	if c.ID == "0" {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	resp, err := h.Services.Update(ctx, c)
+	if err != nil {
+		return nil, errors.Error("Internal DB error")
+	}
+
+	res := models.Response{
+		Cat:        resp,
+		Massage:    "update successful",
 		StatusCode: http.StatusCreated,
 	}
 
--- http/cat/http_test.go
+++ http/cat/http_test.go
@@ -112,3 +112,186 @@ func TestHandler_Get(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_GetByID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	s := services.NewMockServices(ctrl)
+	h := Handler{s}
+	app := gofr.New()
+
+	catModel := models.Cat{
+		ID:   "1",
+		Name: "test_name",
+		Age:  2,
+	}
+
+	testRes := models.Response{
+		Cat:        catModel,
+		Massage:    "successful",
+		StatusCode: http.StatusOK,
+	}
+
+	tests := []struct {
+		desc string
+		id   string
+		res  interface{}
+		mock []*gomock.Call
+		err  error
+	}{{desc: "success", id: catModel.ID, res: testRes,
+		mock: []*gomock.Call{s.EXPECT().GetByID(gomock.Any(), gomock.Any()).Return(catModel, nil)}},
+
+		{desc: "db error", id: catModel.ID, res: nil,
+			mock: []*gomock.Call{s.EXPECT().GetByID(gomock.Any(), catModel.ID).Return(models.Cat{}, errors.Error("Internal DB error"))},
+			err: errors.EntityNotFound{
+				Entity: "cat",
+				ID:     catModel.ID,
+			}},
+
+		{desc: "id is not int", id: "fsef", err: errors.InvalidParam{
+			Param: []string{"id"},
+		}},
+
+		{desc: "id is empty", id: "", err: errors.MissingParam{Param: []string{"id"}}},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "https://cat", nil)
+			req := request.NewHTTPRequest(r)
+			res := responder.NewContextualResponder(w, r)
+			ctx := gofr.NewContext(res, req, app)
+
+			ctx.SetPathParams(map[string]string{
+				"id": tc.id,
+			})
+			result, err := h.GetByID(ctx)
+
+			if !reflect.DeepEqual(err, tc.err) {
+				t.Errorf("test :%v Expected : %v,Got : %v ", i+1, tc.err, err)
+			}
+
+			if !reflect.DeepEqual(tc.res, result) {
+				t.Errorf("test :%v Expected : %v,Got : %v ", i+1, tc.res, result)
+			}
+		})
+	}
+}
+
+func TestHandler_Update(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	s := services.NewMockServices(ctrl)
+	h := Handler{s}
+	app := gofr.New()
+
+	test1 := `{"id":"1","name":"test_name","age":2}`
+	test2 := `{"id":"1","name":"test_name","age":2,"TEST";"TEST"}`
+	test3 := `{"id":"0","name":"test_name","age":2}`
+	test4 := `{"id":"4","name":"test_name","age":2}`
+
+	catModel := models.Cat{
+		ID:   "1",
+		Name: "test_name",
+		Age:  1,
+	}
+
+	tests := []struct {
+		desc string
+		id   string
+		req  []byte
+		mock []*gomock.Call
+		err  error
+	}{{desc: "success", id: "1", req: []byte(test1),
+		mock: []*gomock.Call{s.EXPECT().Update(gomock.Any(), gomock.Any()).Return(catModel, nil)}, err: nil},
+
+		{desc: "id is empty", id: "", err: errors.MissingParam{Param: []string{"id"}}},
+
+		{desc: "id is not int", id: "sef", err: errors.InvalidParam{
+			Param: []string{"id"},
+		}},
+
+		{desc: "ID is 0", id: "0", req: []byte(test3), err: errors.InvalidParam{Param: []string{"id"}}},
+
+		{desc: "unmarsel", id: "1", req: []byte(test2), err: errors.InvalidParam{Param: []string{"body"}}},
+
+		{desc: "db error", id: "1", req: []byte(test4),
+			mock: []*gomock.Call{s.EXPECT().Update(gomock.Any(), gomock.Any()).Return(models.Cat{},
+				errors.Error("Internal DB error"))}, err: errors.Error("Internal DB error")}}
+
+	for i, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "https://cat", bytes.NewReader(tc.req))
+			req := request.NewHTTPRequest(r)
+			res := responder.NewContextualResponder(w, r)
+			ctx := gofr.NewContext(res, req, app)
+
+			ctx.SetPathParams(map[string]string{
+				"id": tc.id,
+			})
+			_, err := h.Update(ctx)
+
+			if !reflect.DeepEqual(err, tc.err) {
+				t.Errorf("test :%v Expected : %v,Got : %v ", i+1, tc.err, err)
+			}
+		})
+	}
+}
+
+func TestHandler_Delete(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	s := services.NewMockServices(ctrl)
+	h := Handler{s}
+	app := gofr.New()
+
+	catModel := models.Cat{
+		ID:   "1",
+		Name: "test_name",
+		Age:  2,
+	}
+
+	testRes := models.Response{
+		Cat:        nil,
+		Massage:    "successful",
+		StatusCode: http.StatusOK,
+	}
+
+	tests := []struct {
+		desc string
+		id   string
+		res  interface{}
+		mock []*gomock.Call
+		err  error
+	}{{desc: "success", id: catModel.ID, res: testRes,
+		mock: []*gomock.Call{s.EXPECT().Delete(gomock.Any(), gomock.Any()).Return(nil)}},
+
+		{desc: "db error", id: catModel.ID, res: nil,
+			mock: []*gomock.Call{s.EXPECT().Delete(gomock.Any(), catModel.ID).Return(errors.Error("Internal DB error"))},
+			err:  errors.DB{Err: errors.Error("Internal DB error")}},
+
+		{desc: "id is not int", id: "fsef", err: errors.InvalidParam{
+			Param: []string{"id"},
+		}},
+
+		{desc: "id is empty", id: "", err: errors.MissingParam{Param: []string{"id"}}},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "https://cat", nil)
+			req := request.NewHTTPRequest(r)
+			res := responder.NewContextualResponder(w, r)
+			ctx := gofr.NewContext(res, req, app)
+
+			ctx.SetPathParams(map[string]string{
+				"id": tc.id,
+			})
+			_, err := h.Delete(ctx)
+
+			if !reflect.DeepEqual(err, tc.err) {
+				t.Errorf("test :%v Expected : %v,Got : %v ", i+1, tc.err, err)
+			}
+		})
+	}
+}
